c6f16eed76e01c63df770347240e087cabb58319
LaszAndrea <79246573+LaszAndrea@users.noreply.github.com>
2024-03-20 15:00:16 +0100

    Add files via upload

---
+++ DJ7PNE_0318/Piros_Auto_feltolt.sql
@@ -0,0 +1,6 @@
+insert into piros_auto values('ABC500', 'Opel Corsa', 'piros', 8, 720000);
+insert into piros_auto values('MCC325', 'Opel Insignia', 'piros', 10, 26800000);
+insert into piros_auto values('BBM104', 'Suzuki Swift','piros', 5, 150000);
+insert into piros_auto values('CHR411', 'Renault Twingo', 'piros', 12, 630000);
+insert into piros_auto values('JRZ932', 'BMW M3', 'fekete', 5,4500000);
+insert into piros_auto values('DER842', 'Seat Ibiza', 'szürke', 14, 45000);
